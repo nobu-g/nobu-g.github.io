9ff704cefcbd84feb24b3e2dfab0cf3e8296024a
nobu-g <ueda-n127@hotmail.co.jp>
2020-12-06 14:59:08 +0900

    adjust header height using css instead of jQuery

--- public/js/init.js
+++ public/js/init.js
@@ -65,12 +65,12 @@ jQuery(document).ready(function ($) {
   /* equal to the browser height.
   ------------------------------------------------------ */
 
-  $('header').css({'height': $(window).height()});
-  $(window).on('resize', function () {
-
-    $('header').css({'height': $(window).height()});
-    $('body').css({'width': $(window).width()})
-  });
+  // $('header').css({'height': $(window).height()});
+  // $(window).on('resize', function () {
+  //
+  //   $('header').css({'height': $(window).height()});
+  //   $('body').css({'width': $(window).width()})
+  // });
 
 
   /*----------------------------------------------------*/
