3ad12555c5e835acf7cc141ddd103886f6e108d3
nobu-g <ueda-n127@hotmail.co.jp>
2021-05-13 21:22:25 +0900

    remove unnecessary js

--- public/js/init.js
+++ public/js/init.js
@@ -6,14 +6,6 @@
 
 jQuery(document).ready(function ($) {
 
-  /*----------------------------------------------------*/
-  /* FitText Settings
-  ------------------------------------------------------ */
-
-  setTimeout(function () {
-    $('h1.responsive-headline').fitText(1, {minFontSize: '40px', maxFontSize: '90px'});
-  }, 100);
-
 
   /*----------------------------------------------------*/
   /* Smooth Scrolling
@@ -31,34 +23,6 @@ jQuery(document).ready(function ($) {
       window.location.hash = target;
     });
   });
-
-
-  /*----------------------------------------------------*/
-  /* Highlight the current section in the navigation bar
-  ------------------------------------------------------*/
-
-  // var sections = $("section");
-  // var navigation_links = $("#nav-wrap a");
-  //
-  // sections.waypoint({
-  //
-  //   handler: function (event, direction) {
-  //
-  //     var active_section;
-  //
-  //     active_section = $(this);
-  //     if (direction === "up") active_section = active_section.prev();
-  //
-  //     var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-  //     console.log(active_link);
-  //
-  //     navigation_links.parent().removeClass("current");
-  //     active_link.parent().addClass("current");
-  //
-  //   },
-  //   offset: '35%'
-  //
-  // });
 
 
   /*----------------------------------------------------*/
@@ -93,50 +57,5 @@ jQuery(document).ready(function ($) {
         nav.addClass('opaque').fadeIn('fast');
       }
     }
-
-  });
-
-
-  /*----------------------------------------------------*/
-  /*	contact form
-  ------------------------------------------------------*/
-
-  $('form#contactForm button.submit').click(function () {
-
-    $('#image-loader').fadeIn();
-
-    var contactName = $('#contactForm #contactName').val();
-    var contactEmail = $('#contactForm #contactEmail').val();
-    var contactSubject = $('#contactForm #contactSubject').val();
-    var contactMessage = $('#contactForm #contactMessage').val();
-
-    var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-      '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-    $.ajax({
-
-      type: "POST",
-      url: "inc/sendEmail.php",
-      data: data,
-      success: function (msg) {
-
-        // Message was sent
-        if (msg == 'OK') {
-          $('#image-loader').fadeOut();
-          $('#message-warning').hide();
-          $('#contactForm').fadeOut();
-          $('#message-success').fadeIn();
-        }
-        // There was an error
-        else {
-          $('#image-loader').fadeOut();
-          $('#message-warning').html(msg);
-          $('#message-warning').fadeIn();
-        }
-
-      }
-
-    });
-    return false;
   });
 });
