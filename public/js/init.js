74fafa97e4b4d9124ca64fd044efcfda08aff42c
nobu-g <ueda-n127@hotmail.co.jp>
2021-05-13 21:27:55 +0900

    smooth scroll without js

--- public/js/init.js
+++ public/js/init.js
@@ -5,26 +5,7 @@
 -----------------------------------------------------------------------------------*/
 
 jQuery(document).ready(function ($) {
-
-
-  /*----------------------------------------------------*/
-  /* Smooth Scrolling
-  ------------------------------------------------------ */
-
-  $('.smoothscroll').on('click', function (e) {
-    e.preventDefault();
-
-    var target = this.hash,
-      $target = $(target);
-
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-    }, 800, 'swing', function () {
-      window.location.hash = target;
-    });
-  });
-
-
+  
   /*----------------------------------------------------*/
   /*	Make sure that #header-background-image height is
   /* equal to the browser height.
