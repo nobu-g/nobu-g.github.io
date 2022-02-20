a169b424f90fe378edc5e90175deb84a79b629e0
nobu-g <ueda-n127@hotmail.co.jp>
2022-02-20 21:03:38 +0900

    move external fonts

--- pages/index.js
+++ pages/index.js
@@ -38,16 +38,6 @@ const Home = () => {
         />
         <meta name="description" content="This is the homepage of Nobuhiro Ueda. About his research and education."/>
         <link rel="icon" href="/favicon.ico"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet"/>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@0;1&family=Open+Sans:wght@400;700&display=swap"
-          rel="stylesheet"/>
       </Head>
       <Header data={config.main} section={section}/>
 
