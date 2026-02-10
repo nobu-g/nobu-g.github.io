2004f16d6822337d4a605fd515c8c2e8e3546f5a
nobu-g <nobu-g@users.noreply.github.com>
2026-02-10 13:07:47 +0000

    deploy: 5ea8f949435f2d4b97c129e5b77e9caba35937df

--- _next/static/chunks/e96da21a0e412904.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/bafddb6099984c02.js",
-  "static/chunks/cd9ccb4fa3345f3d.js",
-  "static/chunks/0daf0721ddf2780f.js",
-  "static/chunks/c105de099730c216.js",
-  "static/chunks/b72525b216492701.js",
-  "static/chunks/f68ac3029454da12.css",
-  "static/chunks/turbopack-01d9497a378d0954.js"
-])
